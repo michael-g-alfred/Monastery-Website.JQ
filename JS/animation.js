12e919d7331adb50f86bfe7db62113126f4857ce
Michael George <Michoolgeorge@gmail.com>
2025-04-24 19:14:05 +0200

    edited JQuery animation

--- JS/animation.js
+++ JS/animation.js
@@ -1,35 +1,35 @@
 $(window).on("load", function () {
-  $("#title01 button").on("click", function (e) {
+  $("#title01").on("click", function (e) {
     e.preventDefault();
     e.stopPropagation();
     $("#content01").slideToggle();
     $(this).toggleClass("active");
   });
-  $("#title02 button").on("click", function (e) {
+  $("#title02").on("click", function (e) {
     e.preventDefault();
     e.stopPropagation();
     $("#content02").slideToggle();
     $(this).toggleClass("active");
   });
-  $("#title03 button").on("click", function (e) {
+  $("#title03").on("click", function (e) {
     e.preventDefault();
     e.stopPropagation();
     $("#content03").slideToggle();
     $(this).toggleClass("active");
   });
-  $("#title04 button").on("click", function (e) {
+  $("#title04").on("click", function (e) {
     e.preventDefault();
     e.stopPropagation();
     $("#content04").slideToggle();
     $(this).toggleClass("active");
   });
-  $("#title05 button").on("click", function (e) {
+  $("#title05").on("click", function (e) {
     e.preventDefault();
     e.stopPropagation();
     $("#content05").slideToggle();
     $(this).toggleClass("active");
   });
-  $("#title06 button").on("click", function (e) {
+  $("#title06").on("click", function (e) {
     e.preventDefault();
     e.stopPropagation();
     $("#content06").slideToggle();
